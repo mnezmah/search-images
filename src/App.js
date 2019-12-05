5e567a74e32cc411d263010148803d420cc755b3
mnezmah <mario.nezmah@gmail.com>
2019-12-05 09:42:06 +0100

    Install more dependencies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Provider } from 'react-redux'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+   <Provider store={store}>
+      <div className="App">
+      </div>
+   </Provider>
   );
 }
 
