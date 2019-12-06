06009d94ee0b2fb737c9d19789ec32ef1107da99
mnezmah <mario.nezmah@gmail.com>
2019-12-06 15:53:12 +0100

    Set style for desktop

--- src/actions/images.js
+++ src/actions/images.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const  GET_IMAGES = 'GET_IMAGES'
+export const GET_IMAGES = 'GET_IMAGES'
 
 function setImages(images) {
   return {
@@ -10,11 +10,10 @@ function setImages(images) {
 }
 
 export function getImages(query) {
-  return function(dispatch) {
+  return function (dispatch) {
     axios.get(`https://pixabay.com/api/?key=${process.env.REACT_APP_API_KEY}&q=${query}&image_type=photo&per_page=9`)
       .then((response) => {
         console.log('response : ', response)
-        console.log('data:', response.data)
         dispatch(setImages(response.data.hits))
       }).catch(error => console.log(error))
   }
